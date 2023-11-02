fdd1d93adac81423e1a148f0dbd2c21be46d3025
HandyDorceus <77027304+itisjamied@users.noreply.github.com>
2023-11-02 17:12:36 -0400

    created clues 1 and 2 templates

---
+++ my-app/src/app/pages/clue1/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center bg-teal-400">
+        
+       <div>Welcome to my Clue 1</div>
+        
+      </main>
+    )
+  }
+  
