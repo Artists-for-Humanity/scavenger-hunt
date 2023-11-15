299fe8e9803fd2e9081e13d1d7750f20120c17e4
HandyDorceus <77027304+itisjamied@users.noreply.github.com>
2023-11-15 12:40:46 -0500

    validation for completing clues in order

--- my-app/src/app/pages/clue1/page.tsx
+++ my-app/src/app/pages/clue1/page.tsx
@@ -1,9 +1,11 @@
+'use client';
 
+import React from 'react';
 import ClueComponent from '../../components/ClueComponent'
-import { completeClue } from '../../utils'
 
 export default function Clue1Page() {
-  completeClue(1);
-  
-  return <ClueComponent clueNumber={1} progressPercentage={10} />;
+  return (
+    <ClueComponent clueNumber={1} />
+  ) 
+
 }
