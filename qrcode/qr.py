a80a18a659bdd11da42968231e35732415e8390f
HandyDorceus <77027304+itisjamied@users.noreply.github.com>
2023-11-21 11:41:14 -0500

    generated codes for local testing

--- qrcode/qr.py
+++ qrcode/qr.py
@@ -2,10 +2,10 @@
 import qrcode
  
 # Data to be encoded
-data = 'https://www.ctechinteractives.pro/pages/clue10'
+data = 'http://192.168.5.138:3000/pages/clue10'
  
 # Encoding data using make() function
 img = qrcode.make(data)
  
 # Saving as an image file
-img.save('Clue10.png') 
+img.save('Clue10-local.png') 
